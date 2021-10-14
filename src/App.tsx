85732e00ab56efe0685a3a31ffc8b71ec28da1c0
Gerald Solano <geraldmacias.gm@gmail.com>
2021-10-14 00:57:08 -0600

    :zap: commit changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "./styles/scss/main.scss";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="bg-img">
+      <header>
+        <div className="menu-btn">
+          <div className="btn-line"></div>
+          <div className="btn-line"></div>
+          <div className="btn-line"></div>
+        </div>
+        <nav className="menu">
+          <div className="menu-branding">
+            <div className="portrait"></div>
+          </div>
+          <ul className="menu-nav">
+            <li className="nav-item current">
+              <a href="" className="nav-link ">
+                {" "}
+                Home{" "}
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="about.html" className="nav-link">
+                {" "}
+                ABOUT ME{" "}
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="work.html" className="nav-link">
+                {" "}
+                MY WORK{" "}
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="contact.html" className="nav-link">
+                {" "}
+                CONTACT{" "}
+              </a>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main id="home">
+        <h1 className="lg-heading">
+          Ing. Gerald <span className="text-secondary">Solano</span>
+        </h1>
+        <h2 className="sm-heading">
+          Web Developer, Programer, Designer & Entrepreneur
+        </h2>
+        <div className="icons">
+          <a href="#!">
+            <i className="fab fa-twitter fa-2x"></i>
+          </a>
+          <a href="">
+            <i className="fab fa-facebook fa-2x"></i>
+          </a>
+          <a href="">
+            <i className="fab fa-github fa-2x"></i>
+          </a>
+          <a href="">
+            <i className="fab fa-linkedin fa-2x"></i>
+          </a>
+        </div>
+      </main>
     </div>
   );
 }
