0cc866705d2211208c8f9f95357b0c57e6cd6db1
Gerald Solano <geraldmacias.gm@gmail.com>
2021-11-02 23:34:37 -0600

    :fire: handler open menu

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,64 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const HomePage: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const clickAction = () => setOpen(!open);
+
+  const menuArray = [
+    {
+      id: 1,
+      path: "/",
+      name: "home",
+    },
+    {
+      id: 2,
+      path: "/aboutMe",
+      name: "about me",
+    },
+    {
+      id: 3,
+      path: "/myWork",
+      name: "my work",
+    },
+    {
+      id: 4,
+      path: "/contact",
+      name: "contact",
+    },
+  ];
+
   return (
     <div id="bg-img">
-      <header>
-        <div className="menu-btn">
+      <section>
+        <div
+          onClick={() => clickAction()}
+          className={`menu-btn ${open && "close"}`}
+        >
           <div className="btn-line"></div>
           <div className="btn-line"></div>
           <div className="btn-line"></div>
         </div>
-        <nav className="menu">
-          <div className="menu-branding">
+        <nav className={`menu ${open && "show"}`}>
+          <div className={`menu-branding ${open && "show"}`}>
             <div className="portrait"></div>
           </div>
-          <ul className="menu-nav">
-            <li className="nav-item current">
-              <a href="" className="nav-link ">
-                {" "}
-                Home{" "}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="about.html" className="nav-link">
-                {" "}
-                ABOUT ME{" "}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="work.html" className="nav-link">
-                {" "}
-                MY WORK{" "}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="contact.html" className="nav-link">
-                {" "}
-                CONTACT{" "}
-              </a>
-            </li>
+          <ul className={`menu-nav ${open && "show"}`}>
+            {menuArray.map((item) => (
+              <li key={item.id} className={`nav-item ${open && "show"}`}>
+                <NavLink
+                  // activeClassName="current"
+                  to={item.path}
+                  className="nav-link "
+                >
+                  {item.name}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </nav>
-      </header>
+      </section>
       <main id="home">
         <h1 className="lg-heading">
           Ing. Gerald <span className="text-secondary">Solano</span>
         </h1>
         <h2 className="sm-heading">Web Developer, Programer & Entrepreneur</h2>
         <div className="icons">
-          <a href="#!">
+          <NavLink to="#!">
             <i className="fab fa-twitter fa-2x"></i>
-          </a>
-          <a href="">
+          </NavLink>
+          <NavLink to="">
             <i className="fab fa-facebook fa-2x"></i>
-          </a>
-          <a href="">
+          </NavLink>
+          <NavLink to="">
             <i className="fab fa-github fa-2x"></i>
-          </a>
-          <a href="">
+          </NavLink>
+          <NavLink to="">
             <i className="fab fa-linkedin fa-2x"></i>
-          </a>
+          </NavLink>
         </div>
       </main>
     </div>
